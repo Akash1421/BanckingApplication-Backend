e1cd40bb0d8d045b7adbe5cfd84beafa0380a324
Akash1421 <akashreddy1421@gmail.com>
2025-08-27 19:54:24 +0530

    first commit

---
+++ src/main/java/com/example/BanckingApplication/Backend/BanckingApplicationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.BanckingApplication.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanckingApplicationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanckingApplicationBackendApplication.class, args);
+	}
+
+}
